d5de971f5c10da760e103d2dc223736dc5d05e9e
Yury Lebedev <quasaryy@gmail.comu>
2023-08-11 12:08:37 +0400

    Layout is ready

--- FruitMatching/ViewController.swift
+++ FruitMatching/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var fruitImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func checkMatchingTapped(_ sender: UIButton) {
+    }
+    
 }
 
